19c281026c21977d295b158e3d433e55f97afde5
Kavya Mandaliya <kavya@kavyas-air.lan>
2019-10-19 21:27:04 -0700

    filtered twitter commits

--- scraper/main.py
+++ scraper/main.py
@@ -1,5 +1,5 @@
-from socialreaper import Twitter
-from socialreaper.tools import to_csv
+from scrap import Twitter
+from scrap.tools import to_csv
 
 twt = Twitter(api_key="your_api", api_secret="your_api_secret", access_token="your-access_token",
     access_token_secret="your-access_token_secret")
@@ -7,4 +7,8 @@ twt = Twitter(api_key="your_api", api_secret="your_api_secret", access_token="yo
 tweets = twt.search("apple", count=500, exclude_replies=True,
     include_retweets=False)
 
-to_csv(list(tweets), filename='apple.csv')
+result = []
+for tweet in tweets:
+    result.append(tweet['full_text'])
+
+# print(result)
