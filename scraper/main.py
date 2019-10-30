b6e4246e6de2d01a65574e69c54ae232e6a8d8df
Kavya Mandaliya <kavya@Kavyas-MacBook-Air.local>
2019-10-29 22:24:22 -0700

    adding data to csv

--- scraper/main.py
+++ scraper/main.py
@@ -1,5 +1,8 @@
-from scrap import Twitter
+from scrap import Twitter, Reddit, Facebook
 from scrap.tools import to_csv
+from scrap.tools import flatten
+import csv
+import numpy
 
 twt = Twitter(api_key="your_api", api_secret="your_api_secret", access_token="your-access_token",
     access_token_secret="your-access_token_secret")
@@ -11,4 +14,6 @@ result = []
 for tweet in tweets:
     result.append(tweet['full_text'])
 
-# print(result)
+a = numpy.array(result)
+# print(a)
+numpy.savetxt('demo.csv', a, fmt='%s')
