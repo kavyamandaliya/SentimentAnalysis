67a9fbd4a8c0e7a5e51d33fd0abeaf8b667e8dce
Kavya Mandaliya <kavya@Kavyas-MacBook-Air.local>
2019-11-12 14:24:31 -0800

    reddit working now

---
+++ scraper/main.py
@@ -0,0 +1,50 @@
+from scrap import Twitter, Reddit, Facebook
+from scrap.tools import to_csv
+from scrap.tools import flatten
+import csv
+import numpy
+
+keyword = "Zayn Malik";
+csvName = keyword + ".csv"
+twt = Twitter(api_key="XXX", api_secret="XXX", access_token="XXX",
+    access_token_secret="XXX")
+
+tweets = twt.search(keyword, count=500, exclude_replies=True,
+    include_retweets=False)
+
+result = []
+for tweet in tweets:
+    result.append(tweet['full_text'])
+
+
+
+rdt = Reddit("XXX", "XXX")
+
+comments = rdt.search(keyword, count=500, order="new", page='',
+    result_type="link", time_period="all")
+
+# Convert nested dictionary into flat dictionary
+comments = [flatten(comment) for comment in comments]
+
+# Sort by comment score
+comments = sorted(comments, key=lambda k: k['data.score'], reverse=True)
+
+# Print the top 10
+for comment in comments[:500]:
+    result.append(comment['data.selftext'])
+    # print(comment)
+
+# fbk = Facebook("EAAepvVwDSngBAPdgRhZA4J60Sa7UdFdAIC4mgCKghXqTstFZBKOxERKLDSTMFo5mLCMmaxuZBpQtiAPWWZCBkvDzBtynDVKdEfZCY9mD0OGxS3nuOukXm5OUCh6sk05cmFmxCelca5x8akdMuF3DQzZAz9qrs1veqQVMK4rIili0ManT9xfmILIx5C0XxtgYXNnUAhNXeQNTgexrEjSZCU8gzLRZCpmkb6yJ1ai8ZCFTwwwZDZD")
+#
+# comments = fbk.page_posts_comments("mcdonalds", post_count=1000,
+#     comment_count=100000)
+#
+# for comment in comments:
+#     print(comment['message'])
+
+#### MAIN ADDING TO THE ARRAY #######
+a = numpy.array(result)
+# print(a)
+numpy.savetxt(csvName, a, fmt='%s')
+# for r in result:e
+#     print(r + "\n")
