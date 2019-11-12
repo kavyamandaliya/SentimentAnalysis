69de3b91e65221f5667be299383d7e08104c8447
Kavya Mandaliya <kavya@Kavyas-MacBook-Air.local>
2019-11-12 15:56:56 -0800

    recent and new SA

--- scraper/main.py
+++ scraper/main.py
@@ -4,35 +4,36 @@ from scrap.tools import flatten
 import csv
 import numpy
 
-keyword = "Zayn Malik";
+keyword = "Dance"
 csvName = keyword + ".csv"
-twt = Twitter(api_key="XXX", api_secret="XXX", access_token="XXX",
-    access_token_secret="XXX")
+csvNewName = keyword + "Recent.csv"
 
-tweets = twt.search(keyword, count=500, exclude_replies=True,
-    include_retweets=False)
+def getInitData(time, fileName):
+    twt = Twitter(api_key="XXX", api_secret="XXX",
+                  access_token="XXX",
+                  access_token_secret="XXX")
+    rdt = Reddit("XXX", "XXX")
+    ##twitter
+    tweets = twt.search(keyword, count=500, exclude_replies=True,
+                        include_retweets=False)
+    result = []
+    for tweet in tweets:
+        result.append(tweet['full_text'])
+    ##reddit
+    comments = rdt.search(keyword, count=500, order="new", page='',
+                          result_type="link", time_period=time)
+    comments = [flatten(comment) for comment in comments]
+    comments = sorted(comments, key=lambda k: k['data.score'], reverse=True)
+    for comment in comments[:500]:
+        result.append(comment['data.selftext'])
+    a = numpy.array(result)
+    # print(a)
+    numpy.savetxt(fileName, a, fmt='%s')
 
-result = []
-for tweet in tweets:
-    result.append(tweet['full_text'])
 
-
-
-rdt = Reddit("XXX", "XXX")
-
-comments = rdt.search(keyword, count=500, order="new", page='',
-    result_type="link", time_period="all")
-
-# Convert nested dictionary into flat dictionary
-comments = [flatten(comment) for comment in comments]
-
-# Sort by comment score
-comments = sorted(comments, key=lambda k: k['data.score'], reverse=True)
-
-# Print the top 10
-for comment in comments[:500]:
-    result.append(comment['data.selftext'])
-    # print(comment)
+#####   MAIN METHOD  ######
+getInitData("month", csvName)
+getInitData("hour", csvNewName)
 
 # fbk = Facebook("EAAepvVwDSngBAPdgRhZA4J60Sa7UdFdAIC4mgCKghXqTstFZBKOxERKLDSTMFo5mLCMmaxuZBpQtiAPWWZCBkvDzBtynDVKdEfZCY9mD0OGxS3nuOukXm5OUCh6sk05cmFmxCelca5x8akdMuF3DQzZAz9qrs1veqQVMK4rIili0ManT9xfmILIx5C0XxtgYXNnUAhNXeQNTgexrEjSZCU8gzLRZCpmkb6yJ1ai8ZCFTwwwZDZD")
 #
@@ -42,9 +43,5 @@ for comment in comments[:500]:
 # for comment in comments:
 #     print(comment['message'])
 
-#### MAIN ADDING TO THE ARRAY #######
-a = numpy.array(result)
-# print(a)
-numpy.savetxt(csvName, a, fmt='%s')
 # for r in result:e
 #     print(r + "\n")
--- sentiments.py
+++ sentiments.py
@@ -4,28 +4,40 @@ import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 nltk.downloader.download('vader_lexicon')
 
+keyword = ""
+filePath = "scraper/" + keyword + ".csv"
+filePathRecent = "scraper/" + keyword + "Recent.csv"
 # text1 = 'I am very irritated, cause the code is not working, and it is giving several errors. The movie was a total waste of time. And it went on for so long that I got bored'
 # sum1 = TextBlob(text1)
 # sent1 = sum1.sentiment.polarity
 # print(sent1)
 
-sid = SentimentIntensityAnalyzer()
-data = pd.read_csv("scraper/Nike.csv",header=None,error_bad_lines=False,quotechar=None, quoting=3)
-# print(data)
+def getSentiments(txt):
+    positive = 0
+    negative = 0
+    neutral = 0
+    print(len(texts))
+    for t in texts:
+        #     print(t)
+        sentiment = sid.polarity_scores(t)
+        for k in sorted(sentiment):
+            if sentiment[k] > 0:
+                positive += 1
+            if sentiment[k] < 0:
+                negative += 1
+            if sentiment[k] == 0.0:
+                neutral += 1
+    print("Negative: " + str(negative))
+    print("Positive: " + str(positive))
+    print("Neutral: " + str(neutral))
 
+
+####### MAIN FUNCTION ##########
+sid = SentimentIntensityAnalyzer()
+import nltk
+data = pd.read_csv(filePath, header=None,error_bad_lines=False,quotechar=None, quoting=3)
+dataRecent = pd.read_csv(filePathRecent,header=None,error_bad_lines=False,quotechar=None, quoting=3)
 texts = data[0]
-positive = 0
-negative = 0
-neutral = 0
-for t in texts:
-    sentiment = sid.polarity_scores(t)
-    for k in sorted(sentiment):
-        if sentiment[k] > 0:
-            positive += 1
-        if sentiment[k] < 0:
-            negative += 1
-        if sentiment[k] == 0.0:
-            neutral += 1
-print("Negative: " + str(negative))
-print("Positive: " + str(positive))
-print("Neutral: " + str(neutral))
+textsRecent = dataRecent[0]
+getSentiments(texts)
+getSentiments(textsRecent)
