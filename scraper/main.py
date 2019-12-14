7e8466cc9ff1a97a19495bbb5b6a8598e614676c
Kavya Mandaliya <kavya@Kavyas-MacBook-Air.local>
2019-12-14 12:58:13 -0800

    DONE

--- scraper/main.py
+++ scraper/main.py
@@ -4,44 +4,56 @@ from scrap.tools import flatten
 import csv
 import numpy
 
-keyword = "Dance"
+keyword = "Iphone"
 csvName = keyword + ".csv"
 csvNewName = keyword + "Recent.csv"
+twt = ""
+rdt = ""
 
-def getInitData(time, fileName):
+##BLURRED OUT THE API KEYS FOR PRIVACY CONCERNS 
+def getInitData(fileName):
     twt = Twitter(api_key="XXX", api_secret="XXX",
                   access_token="XXX",
                   access_token_secret="XXX")
     rdt = Reddit("XXX", "XXX")
     ##twitter
     tweets = twt.search(keyword, count=500, exclude_replies=True,
-                        include_retweets=False)
+        include_retweets=False)
     result = []
-    # for tweet in tweets:
-    #     result.append(tweet['full_text'])
+    for tweet in tweets:
+        result.append(tweet['full_text'])
     ##reddit
-    comments = rdt.search(keyword, count=500, order="new", page='',
-                          result_type="link", time_period=time)
+    comments = rdt.search(keyword, count=20000, order="new", page='',
+        result_type="link", time_period="year")
     comments = [flatten(comment) for comment in comments]
     comments = sorted(comments, key=lambda k: k['data.score'], reverse=True)
-    for comment in comments[:500]:
+    for comment in comments:
         result.append(comment['data.selftext'])
     a = numpy.array(result)
     # print(a)
     numpy.savetxt(fileName, a, fmt='%s')
 
+def getNewData(fileName):
+    twt = Twitter(api_key="XXX", api_secret="XXX",
+                  access_token="XXX",
+                  access_token_secret="XXX")
+    rdt = Reddit("XXX", "XXX")
+    ##twitter
+    tweets = twt.search(keyword, count=500, exclude_replies=True,
+        include_retweets=False)
+    result = []
+    for tweet in tweets:
+        result.append(tweet['full_text'])
+    ##reddit
+    comments = rdt.search(keyword, count=20000, order="new", page='',
+        result_type="link", time_period="month")
+    comments = [flatten(comment) for comment in comments]
+    for comment in comments:
+        result.append(comment['data.selftext'])
+    a = numpy.array(result)
+    # print(a)
+    numpy.savetxt(fileName, a, fmt='%s')
 
 #####   MAIN METHOD  ######
-getInitData("month", csvName)
-getInitData("hour", csvNewName)
-
-# fbk = Facebook("EAAepvVwDSngBAPdgRhZA4J60Sa7UdFdAIC4mgCKghXqTstFZBKOxERKLDSTMFo5mLCMmaxuZBpQtiAPWWZCBkvDzBtynDVKdEfZCY9mD0OGxS3nuOukXm5OUCh6sk05cmFmxCelca5x8akdMuF3DQzZAz9qrs1veqQVMK4rIili0ManT9xfmILIx5C0XxtgYXNnUAhNXeQNTgexrEjSZCU8gzLRZCpmkb6yJ1ai8ZCFTwwwZDZD")
-#
-# comments = fbk.page_posts_comments("mcdonalds", post_count=1000,
-#     comment_count=100000)
-#
-# for comment in comments:
-#     print(comment['message'])
-
-# for r in result:e
-#     print(r + "\n")
+getInitData(csvName)
+getNewData(csvNewName)
